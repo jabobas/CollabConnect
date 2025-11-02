feff45aa9477743d9bfb20262da0ec1c1f8a1f9d
lmatheson2026 <lucas.matheson@maine.edu>
2025-11-02 14:08:32 -0500

    Department and WorkHistory table creation scripts added.

---
+++ Backend/SQL Queries/Tables/Department.sql
@@ -0,0 +1,6 @@
+Create table Department (
+    department_phone VARCHAR(20) NOT NULL,
+    department_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    department_email VARCHAR(150) NOT NULL,
+    department_name VARCHAR(150) NULL
+);
