c87db6d98a3afc436bd0cdd727672028f58ad7dc
lmatheson2026 <lucas.matheson@maine.edu>
2025-11-18 10:18:57 -0500

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,7 @@ import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+
 import Contacts from "./scenes/contacts"
 
 function App() {
